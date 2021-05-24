c600619fe8b7b377dfc3e95eede5dae6300238ce
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-24 11:23:48 -0700

    corrects namespace Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com> Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com> Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

--- AnniesAnesthesia/Controllers/PatientsController.cs
+++ AnniesAnesthesia/Controllers/PatientsController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using AnniesAnesthesia.Models;
 
-namespace AnniesAnesthesialers
+namespace AnniesAnesthesia
 {
   public class PatientsController : Controller
   {
