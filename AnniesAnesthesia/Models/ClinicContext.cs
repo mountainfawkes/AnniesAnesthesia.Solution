3dc763fbefac21f0de86143e6cf6516d20e07e85
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-24 10:51:18 -0700

    applies template to project files Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com> Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com> Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

---
+++ AnniesAnesthesia/Models/ClinicContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnniesAnesthesia.Models
+{
+  public class ClinicContext : DbContext
+  {
+    public virtual DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<DoctorPatient> DoctorPatient { get; set; }
+
+    public ClinicContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
