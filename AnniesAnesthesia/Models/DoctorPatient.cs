8f70be342eed1dbea12eba61545312bb138927fb
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-24 14:05:04 -0700

    creates Home and Patients views
    Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com>
    Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com>
    Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

--- AnniesAnesthesia/Models/DoctorPatient.cs
+++ AnniesAnesthesia/Models/DoctorPatient.cs
@@ -6,11 +6,16 @@ namespace AnniesAnesthesia.Models
     public int PatientId { get; set; }
     public int DoctorId { get; set; }
 <<<<<<< HEAD
+<<<<<<< HEAD
     public virtual Doctor Doctor { get; set; }
     public virtual Patient Patient { get; set; }
 =======
     public Doctor Doctor { get; set; }
     public Patient Patient { get; set; }
 >>>>>>> e450e1f (completes and implements CRUD functionality Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com> Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com> Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>)
+=======
+    public virtual Doctor Doctor { get; set; }
+    public virtual Patient Patient { get; set; }
+>>>>>>> 54910cc (creates Home and Patients views)
   }
 }
